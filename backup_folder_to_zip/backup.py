463c8c093f046250a072546586da561832f46ba8
dari <hernandezdarifrancisco@gmail.com>
2020-12-22 14:04:54 -0600

    End OOP and update README
    Convert last code to OOP with a main class
    Test the main class
    Upodate README.m: "How to use"

--- backup_folder_to_zip/backup.py
+++ backup_folder_to_zip/backup.py
@@ -1,67 +1,88 @@
 #! python3
-# Copies an entire folder and its contains into
-# a zip file whose filename increments. 
 
-import zipfile, os, sys
+import zipfile, os, sys, errno
 
-folderToZip = ''
-folderDestination = ''
+class Backup (): 
+    """ 
+    Main class. Copies an entire folder and its contains into a zip file whose 
+    filename increments.
+    """
 
-# Red the terminal
-helpMenssage = 'Write the folder to backup in zip, and opcional the destination \
-        folder to the zip file (example: main.py "user/myfoldertobackup" "user/folderdestination")'
-if len(sys.argv) == 1: 
-    print ('The program need more arguments. ' + helpMenssage)
-    sys.exit()
-elif len(sys.argv) == 2:  
-    folderToZip = sys.argv[1]
-    folderDestination = os.path.dirname(folderToZip)
-elif len(sys.argv) == 3:  
-    folderToZip = sys.argv[1]
-    folderDestination = sys.argv[2]
-else: 
-    print ('To much argument. ' + helpMenssage)
-    sys.exit()
+    def __init__ (self, folderToZip, folderDestination): 
+        """ 
+        Constructor of the class. 
+        Get the folder to backup in zip file, and the folder estinarion of the zip file
+        """
 
-def backupToZip (folder, destination):
-    """ Backup the antire contents of "folder" into a ZIP file """
+        self.folderToZip = folderToZip
+        self.folderDestination = folderDestination        
 
-    # Verify the paths
-    if not os.path.exists (folderToZip) or not os.path.exists (folderDestination): 
-        print ('Check your paths')
-        sys.exit()
+        self.__verify_paths()
+
+        self.zipFilename = self.__get_name_zip_file()
+
+        self.__backup_folder()
+
         
-    folder =  folder
+    def __verify_paths (self):
+        """
+        Verify is the from and the to path exist in the pc
+        """ 
 
-    # Figure out the filename this code should use based on 
-    # what files already exist
-    number = 1
-    while True: 
-        os.chdir (destination)
-        zipFilename = os.path.basename(folder) + '_' + str(number) + '.zip'
-        if not os.path.exists(zipFilename):
-            break
-        number += 1
-    
-    #Create the zip file
-    print ('Creating %s...' % (zipFilename))
-    #os.chdir('/home/dari/develop/learning/programación/python/ejercicios/automate_the_boring_stuff_with_python_new/9_organicing_files')
-    backupZip = zipfile.ZipFile(zipFilename, 'w')
+        # Verify the paths
+        if not os.path.exists (self.folderToZip): 
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), self.folderToZip)
+        
+        if not os.path.exists (self.folderDestination): 
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), self.folderDestination)
 
-    #Walk the entire folder tree and compress the files in each folder.
-    os.chdir (folder)
-    for foldername, subfolders, filesnames in os.walk('.'):
-        print ('Adding files in %s...' %(foldername))
-        # Add the current folder to the zip file
-        backupZip.write(foldername)
-        # Add all files in this folder, to the Zip file
-        for filename in filesnames: 
-            newBase = os.path.basename(folder) + '_' #Checks the starts of the file
-            if filename.startswith(newBase) and filename.endswith('.zip'):
-                continue # Dont backup the backup zip files
-            backupZip.write(os.path.join(foldername, filename))
-    backupZip.close()
-    print ('Done.')
 
-backupToZip(folderToZip, folderDestination)
+    def __get_name_zip_file (self): 
+        """
+        Generate and return the name of the zip file. 
+        If file already exist, increase the number 
+        """
+
+        number = 1
+        while True: 
+            os.chdir (self.folderDestination)
+            zipFilename = os.path.basename(self.folderToZip) + '_' + str(number) + '.zip'
+            if not os.path.exists(zipFilename):
+                break
+            number += 1
+        
+        return zipFilename
+
+
+    def __backup_folder (self): 
+        """ 
+        Backup the folder
+        """
+
+        print ('Creating "{}" file...'.format (self.zipFilename))
+
+        # Make and open file
+        os.chdir (self.folderDestination)
+        backupZip = zipfile.ZipFile(self.zipFilename, 'w')
+
+
+        #Walk the entire folder tree and compress the files in each folder.
+        os.chdir (self.folderToZip)
+        for foldername, subfolders, filesnames in os.walk('.'):
+
+            # Add the current folder to the zip file
+            backupZip.write(foldername)
+
+            # Add all files in this folder, to the Zip file
+            for filename in filesnames: 
+                newBase = os.path.basename(self.folderToZip) + '_' 
+                
+                #Checks the starts of the file
+                if filename.startswith(newBase) and filename.endswith('.zip'):
+                    continue # Dont backup the backup zip files
+
+                backupZip.write(os.path.join(foldername, filename))
+
+        backupZip.close()
+        print ('Done.')
 
